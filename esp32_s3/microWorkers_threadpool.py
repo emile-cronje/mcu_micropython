25d291b89805b473dbbd86957911898b3cda4cdd
Emile Cronje <emile.cronje@gmail.com>
2026-01-02 13:49:19 +0200

    locking ok

--- esp32_s3/microWorkers_threadpool.py
+++ esp32_s3/microWorkers_threadpool.py
@@ -10,12 +10,13 @@ from ramblock import RAMBlockDevExt, RAMBlockDev
 
 finalResult = True
 rbDir = '/rb'
-_entityCount = 1000
+_entityCount = 10
 sleepFor = 0
+lock = _thread.allocate_lock()
    
 def fileTest(id, targetDir):
     global finalResult
-    print("In fileTest for id...", id)
+    #print("In fileTest for id...", id)
     sleep(2)
     
     counter = 0
@@ -23,7 +24,7 @@ def fileTest(id, targetDir):
     for count in range(_entityCount):
         counter += 1
         s = "Thread: " + str(id) + " Count: " + str(count + 1)
-        print(s)
+#        print(s)
             
         line = 'abcdefghijklmnopqrstuvwxyz_' + str(count) + '\n'
         lines = line * (_entityCount + count)
@@ -58,8 +59,6 @@ def fileTest(id, targetDir):
         log('Verifying data read back')
         success = True
         
-        result_long = '1000'
-        
         if result_long == ''.join((lines, short, lines)):
             log('Large file Pass')
         else:
@@ -72,11 +71,16 @@ def fileTest(id, targetDir):
             log('Small file Fail')
             success = False
 
-        if (_entityCount == 1) and (result_short == (short + 'a')):
+        # force fail
+        if (id == -1):
+            result_short = 'a'
+        
+        if (result_short == short):
             log('Small file Pass')
         else:
             log('Small file Fail')
             success = False
+            #print("Forced failure...")
 
     sleep(sleepFor)
     
@@ -86,37 +90,24 @@ def fileTest(id, targetDir):
     if (success == False):
         finalResult = False
         
-    print('Tests', 'passed' if success else 'failed', str(id))
+    print('Tests', 'passed:' if success else 'failed:', str(id))
 
 def writeToFile(fileName, data, targetDir = None):
     fileName += '.txt'
-    print(f"Targetdir: {targetDir}")
+    #print(f"Targetdir: {targetDir}")
     
     targetFile = '/rb/' + fileName
-    print("Targetfile...:" + targetFile)
+    #print("Targetfile...:" + targetFile)
     
     with open(targetFile,'w') as f:
-        print("opened " + targetFile + "\r\n")        
+        #print("opened " + targetFile + "\r\n")        
         n = f.write(data)
-        print("writing to file.." + fileName + "\r\n")
+        #print("writing to file.." + fileName + "\r\n")
         log(str(n) + ' bytes written')
 
 def log(s):
+    return
     print(s)
-    
-def jobA(jobName, jobArg):
-    global lock
-    
-    #lock.acquire()
-    fileTest(jobArg, '/rb')
-    writeToFile(jobName, str(time.time_ns()), '/rb')
-    print("jobArg:", jobArg)
-    sleep(.2)
-    #lock.release()        
-    return '%s:OK:1s' % jobName
-
-def jobFinished(jobName, jobArg, jobResult):
-    print('Job %s finished (%s)' % (jobName, jobResult))
 
 def free(full=False):
   F = gc.mem_free()
@@ -126,25 +117,45 @@ def free(full=False):
   if not full: return P
   else : return ('Total:{0} Free:{1} ({2})'.format(T,F,P))
 
+def jobA(jobName, jobArg):
+    global lock
+   
+    try:
+        lock.acquire()        
+        fileTest(jobArg, '/rb')
+        writeToFile(jobName, str(time.time_ns()), '/rb')
+        lock.release()        
+    except Exception as e:
+        print("Error: " + str(e))
+        raise
+
+    #print("jobArg:", jobArg)
+    #sleep(.2)
+    
+    return '%s:OK:1s' % jobName
+
+def jobFinished(jobName, jobArg, jobResult):
+    print('Job %s finished (%s)' % (jobName, jobResult))
+
 machine.freq(240000000)
 bdev = RAMBlockDevExt(512, 2000)
 os.VfsFat.mkfs(bdev)
 os.mount(bdev, rbDir)
 
-workers = MicroWorkers(workersCount=10, workersStackSize=1024)
-lock = _thread.allocate_lock()
+count = 10
+workers = MicroWorkers(workersCount=15, workersStackSize=1024)
 start = time.time()
 
-for x in range(10):
+for x in range(count):
     workers.AddJob('JobA_%s' % x, jobA, arg=x + 1, onFinished=jobFinished)
     workers.AddJob('JobB_%s' % x, jobA, arg=x + 2, onFinished=jobFinished)
     workers.AddJob('JobC_%s' % x, jobA, arg=x + 3, onFinished=jobFinished)    
     
 while workers.IsWorking:
-    #print("Jobs in progress..." + str(workers.JobsInProcess))
-    #print("memory : " + free())    
-    #print("Jobs in queue..." + str(workers.JobsInQueue))    
-    sleep(1)
+    print("Jobs in progress..." + str(workers.JobsInProcess))
+    print("memory : " + free())    
+    print("Jobs in queue..." + str(workers.JobsInQueue))    
+    sleep(5)
     
 end = time.time()    
 print(end - start)
